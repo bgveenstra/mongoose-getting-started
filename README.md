98e5bd7f4da264933fe5aad941700801b075f90b
bgveenstra <b.g.veenstra@gmail.com>
2016-03-29 11:41:03 -0700

    starter code and instructions

---
+++ README.md
@@ -0,0 +1,26 @@
+# Object Mapping
+
+We're about to start working with a database! Yay!
+
+![](https://media.giphy.com/media/D76YOxpdvXne8/giphy.gif)
+
+Remember how our data got reset every time we restarted our servers over the past few days?  Databases help us store data *persistently* longer than we could if we just stored data in an array in our server code.  
+
+We'll see a few different types of databases during WDI that store data in different ways. Our first database is MongoDB. MongoDB stores data in BSON, a format that looks a lot like JSON. The other database we'll look at later, PostgreSQL, will store data in a much more broken up, strictly structured way.  
+
+**The way a database stores data isn't always the most convenient way for us to work with the data.**   When we want database data in a working app, we'll add a layer between the database and our code. The layer will help translate from raw database data to objects like we're used to working with. This extra layer is called an Object _something_ Mapper. MongoDB refers to its data as "documents," so with MongoDB we'll use an Object Document Mapper (or ODM), called Mongoose.
+
+## Explore MongoDB and Mongoose
+
+1. Read only the first page of MongoDB's [Introduction](https://docs.mongodb.org/getting-started/cpp/introduction/). You should get an idea of what a MongoDB _document_ is and what a MongoDB _collection_ is.  The rest of the MongoDB docs are good and interesting, but mongoose's syntax is a little different, so let's move on.
+
+1. Fork and clone this repository. Change directories into your local copy.  We're going to play with mongoose, so `npm install mongoose`.  Of course we'll need MongoDB running, so run `mongod`. Notice that `mongod` takes over this tab of your terminal.
+
+1. We need to leave `mongod` running, so open a new terminal tab by pressing command and t. The new tab should open in the same directory you were at.  Run the `gettingstarted.js` file by entering `node gettingstarted.js`.  You should see some kitten stuff happen in your terminal.
+
+1. Let's investigate the kitten stuff!  Some of the comments inside `gettingstarted.js` start with `// ??`.  Answer the questions in those comments, or make the code additions they suggest.  Use the following resources:
+
+  * Read over [mongoose's quick start guide](http://mongoosejs.com/docs/).    
+  * Read the first few sections of Mongoose's [guide](http://mongoosejs.com/docs/guide.html), up through the section on creating a model. If you're super curious and have a few more minutes, read about creating instance methods with `someSchema.methods` and static methods with `someSchema.statics`. We don't recommend reading beyond there for now.
+
+1. Add, commit and push your changes for this repo, then submit a pull request.
---
+++ gettingstarted.js
@@ -0,0 +1,88 @@
+// See http://mongoosejs.com/docs/
+// Before you start:
+  // npm install mongoose from your terminal
+  // in your terminal, run mongod
+// To run this file:
+  // leave mongod running
+  // open a new tab in your terminal with command t
+  // in your terminal, run node gettingstarted.js
+  // after you see the output, you can use control c to kill this node app
+
+// get access to code from the mongoose module with require. save as a variable
+var mongoose = require('mongoose');
+
+// use mongoose to connect to a 'test' database on this computer
+mongoose.connect('mongodb://localhost/test');
+
+// ?? Why do we set up a schema, or what is a schema used for in mongoose?
+var kittySchema = mongoose.Schema({
+  // ?? Add one more attribute of your choice to this kitty Schema object!
+  name: String
+});
+
+
+// speak has been rewritten without the ternary operator tomfoolery
+kittySchema.methods.speak = function () {
+  // ?? What does `this` refer to inside kittySchema.methods.speak?
+  var greeting;
+  if (this.name){
+    greeting = "Meow name is " + this.name;
+  } else {
+    greeting = "I don't have a name";
+  }
+  console.log(greeting);
+};
+
+// ?? What is a model used for with Mongoose? How is it different from a schema?
+Kitten = mongoose.model('Kitten', kittySchema);
+
+
+var fluffy = new Kitten({ name: 'fluffy' });
+console.log("Here's a brand new kitten: ", fluffy);
+// speak was added to kittenSchema.methods, so here's how we call it:
+fluffy.speak(); // "Meow name is fluffy"
+
+// ?? Why do we need to save the new kitten? What happens if you comment this part out?
+fluffy.save(function (err, fluffy) {
+  if (err) return console.error(err);
+  console.log('You saved ' + fluffy.name + '! ', fluffy);
+  // ?? How does fluffy's object look different after it's been saved?
+});
+
+
+// ?? Make a new kitten with a different name, also using the attribute you added to the kitty schema.
+// ?? Before you move on, run `gettingstarted.js` again. Does your new kitten show up on the list of all kittens?
+
+
+// ?? Save your new kitten. Does your new kitten show up on the list of all kittens now?
+
+
+// find some kittens!
+// The first {} argument here lets us filter down which kittens we're looking for.
+// An empty object (or leaving the object off) means find all kittens!
+Kitten.find({}, function (err, kittens) {
+  if (err) return console.error(err);
+  console.log('Here are all the kittens!');
+  console.log(kittens);
+});
+
+// finds only kittens named fluffy
+Kitten.find({name: 'fluffy'}, function (err, kittens) {
+  if (err) return console.error(err);
+  console.log('Here are all the kittens named fluffy!');
+  console.log(kittens);
+});
+
+// ?? Make another call to Kitten.find, but filter down only kittens with your kitten's name!
+
+
+// Set up the connection to close when we kill our node app
+// by listening for an interrupt singal ('SIGINT') from the terminal.
+// This lets us avoid putting all our code inside a giant callback function.
+// Don't worry about details of this code; focus on mongoose stuff.
+process.on('SIGINT', function() {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected on node app termination');
+    process.exit(0);
+  });
+});
